d83a34718596a18f91cf412f42ab43a3866ad36d
rcmlz <rcmlz@github.com>
2025-09-14 20:37:57 +0200

    Fix

--- postBuild.d/02_rakudo-jupyter-kernel.sh
+++ postBuild.d/02_rakudo-jupyter-kernel.sh
@@ -7,4 +7,4 @@ export ZEF_FETCH_DEGREE=4
 # instruction taken from https://github.com/antononcube/Raku-Jupyter-Chatbook
 zef install --/test --deps-only Jupyter::Chatbook
 zef install --/test Jupyter::Chatbook
-jupyter-chatbook.raku --generate-config
+jupyter-chatbook --generate-config

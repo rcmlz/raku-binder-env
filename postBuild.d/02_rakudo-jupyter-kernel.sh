cd456ab48bf3dbe689fd65397addec9d54e7deaf
rcmlz <rcmlz@github.com>
2025-09-14 19:53:37 +0200

    Initial

--- postBuild.d/02_rakudo-jupyter-kernel.sh
+++ postBuild.d/02_rakudo-jupyter-kernel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source ~/start
 
-# instruction taken from https://github.com/bduggan/p6-jupyter-kernel
-zef install Jupyter::Kernel
-jupyter-kernel.raku --generate-config
+export ZEF_TEST_DEGREE=4
+export ZEF_FETCH_DEGREE=4
+
+# instruction taken from https://github.com/antononcube/Raku-Jupyter-Chatbook
+zef install --/test --deps-only Jupyter::Chatbook
+zef install --/test Jupyter::Chatbook
+jupyter-chatbook.raku --generate-config
--- postBuild.d/04_raku-modules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ~/start
-
-#zef install SVG::Plot::Pie Metropolis Telemetry Log::Async Test::Fuzz

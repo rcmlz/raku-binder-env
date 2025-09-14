0b50f8b5a6bd08868f8a9d3c6d777bd5bcf42b50
rcmlz <rcmlz@github.com>
2025-09-14 20:26:29 +0200

    Update

--- postBuild.d/01_raku-brew.sh
+++ postBuild.d/01_raku-brew.sh
@@ -3,3 +3,15 @@
 curl https://rakubrew.org/install-on-perl.sh | sh
 
 echo 'eval "$(/home/jovyan/.rakubrew/bin/rakubrew init Bash)"' >> ~/start
+echo 'eval "$(/home/jovyan/.rakubrew/bin/rakubrew init Bash)"' >> ~/.bashrc
+
+eval "$(/home/jovyan/.rakubrew/bin/rakubrew init Bash)"
+
+# list available versions
+rakubrew available
+
+# download and install the latest Rakudo on MoarVM version
+rakubrew download
+
+# show current version
+raku -e 'say "Now running {$*RAKU.compiler.version}!"'

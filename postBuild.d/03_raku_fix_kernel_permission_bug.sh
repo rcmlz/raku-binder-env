03b92b2200fb2f1a6fd9dd504453177f6eafd567
rcmlz <rcmlz@github.com>
2025-09-18 09:30:04 +0200

    Fix permission bug

---
+++ postBuild.d/03_raku_fix_kernel_permission_bug.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source ~/start
+
+# Fixing permission bug for Raku Jupyter kernel
+# See https://github.com/antononcube/Raku-Jupyter-Chatbook/issues/7
+
+chmod +x $HOME/.rakubrew/versions/*/share/perl6/site/bin/*.raku

213f575f5b958ed6e24c58898c48685a5d60d51a
rcmlz <rcmlz@github.com>
2025-09-18 08:56:17 +0200

    Fix auth

--- postBuild.d/02_rakudo-jupyter-kernel.sh
+++ postBuild.d/02_rakudo-jupyter-kernel.sh
@@ -4,13 +4,15 @@ source ~/start
 export ZEF_TEST_DEGREE=4
 export ZEF_FETCH_DEGREE=4
 
+export $PACKAGE_NAME='Jupyter::Chatbook:auth<zef:antononcube>'
+
 echo "instruction taken from https://github.com/antononcube/Raku-Jupyter-Chatbook"
 
-echo "Installing Jupyter::Chatbook dependencies only"
-zef install --/test --deps-only Jupyter::Chatbook
+echo "Installing $PACKAGE_NAME dependencies only"
+zef install --/test --deps-only "$PACKAGE_NAME"
 
-echo "Installing Jupyter::Chatbook kernel"
-zef install --/test Jupyter::Chatbook
+echo "Installing $PACKAGE_NAME kernel"
+zef install --/test "$PACKAGE_NAME"
 
-echo "Generating Jupyter::Chatbook config"
+echo "Generating $PACKAGE_NAME config"
 jupyter-chatbook --generate-config

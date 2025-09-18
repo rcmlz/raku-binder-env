83d76cd8a4cda168230ba053501985661cfbb4dc
rcmlz <rcmlz@github.com>
2025-09-18 10:24:23 +0200

    Fix

--- postBuild.d/02_raku-jupyter-kernel.sh
+++ postBuild.d/02_raku-jupyter-kernel.sh
@@ -14,5 +14,7 @@ zef install --/test --deps-only "$PACKAGE_NAME"
 echo "Installing $PACKAGE_NAME kernel"
 zef install --/test "$PACKAGE_NAME"
 
+rakubrew mode shim
+
 echo "Generating $PACKAGE_NAME config"
-jupyter-chatbook --generate-config
+jupyter-chatbook.raku --generate-config

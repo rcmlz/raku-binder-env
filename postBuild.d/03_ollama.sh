6a922337f4ceb788f52cf6a17d24924d80662f0e
rcmlz <rcmlz@github.com>
2026-01-15 12:56:57 +0100

    Ollama

--- postBuild.d/03_ollama.sh
+++ postBuild.d/03_ollama.sh
@@ -3,11 +3,12 @@ source ~/start
 
 echo "instruction taken from https://docs.ollama.com/linux"
 
-curl -fsSL https://ollama.com/download/ollama-linux-amd64.tar.zst  | tar --zstd -xvf -
+mkdir -p ollama
+curl -fsSL https://ollama.com/download/ollama-linux-amd64.tar.zst  | tar -C ollama --zstd -xvf -
 
 #running as local user:    
-echo '/home/jovyan/bin/ollama serve &' >> ~/start
-bin/ollama serve &
+echo '/home/jovyan/ollama/bin/ollama serve &' >> ~/start
+ollama/bin/ollama serve &
 
 # pull small model
-bin/ollama pull gemma3:270m
+ollama/bin/ollama pull gemma3:270m

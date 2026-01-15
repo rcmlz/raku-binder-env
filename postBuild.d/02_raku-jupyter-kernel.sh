19baca770f1bf97e6c7370f8edc7e11d984c510c
rcmlz <rcmlz@github.com>
2026-01-15 10:53:01 +0100

    Adding Raku apt

--- postBuild.d/02_raku-jupyter-kernel.sh
+++ postBuild.d/02_raku-jupyter-kernel.sh
@@ -4,7 +4,7 @@ source ~/start
 export ZEF_TEST_DEGREE=4
 export ZEF_FETCH_DEGREE=4
 
-export PACKAGE_NAME='Jupyter::Chatbook:auth<zef:antononcube>'
+export PACKAGE_NAME='Jupyter::Chatbook:ver<0.3.6>:auth<zef:antononcube>:api<1>'
 
 echo "instruction taken from https://github.com/antononcube/Raku-Jupyter-Chatbook"
 
@@ -17,15 +17,16 @@ zef install --/test "$PACKAGE_NAME"
 echo "Generating $PACKAGE_NAME config"
 jupyter-chatbook --generate-config
 
-echo "Patching kernel.json - removing the .raku"
+#echo "Patching kernel.json - removing the .raku"
 # cp $HOME/.local/share/jupyter/kernels/raku/kernel.json $HOME/postBuild.d/
 # vi $HOME/postBuild.d/kernel.json
 # diff --unified $HOME/.local/share/jupyter/kernels/raku/kernel.json kernel.json > kernel.json.patch
 # echo "\n" >> $HOME/postBuild.d/kernel.json.patch
-patch -d $HOME/.local/share/jupyter/kernels/raku/ < $HOME/postBuild.d/kernel.json.patch
+#patch -d $HOME/.local/share/jupyter/kernels/raku/ < $HOME/postBuild.d/kernel.json.patch
 
-echo "patched kernel.json"
-cat $HOME/.local/share/jupyter/kernels/raku/kernel.json
+#echo "patched kernel.json"
+#cat $HOME/.local/share/jupyter/kernels/raku/kernel.json
 
 # more libraries that might be useful
 zef install --/test SVG::Plot Text::Plot
+

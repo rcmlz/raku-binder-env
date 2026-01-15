87e371a49b20250bb6176b9501f51e42ba3a169c
rcmlz <rcmlz@github.com>
2026-01-15 11:51:46 +0100

    Nix

--- default.nix
+++ default.nix
@@ -1,15 +1,5 @@
-let
-  # Pinning nixpkgs to specific release
-  # To get sha256 use "nix-prefetch-git <url> --rev <commit>"
-  # nix-prefetch-git https://github.com/NixOS/nixpkgs.git --rev 25.11
-  commitRev="871b9fd269ff6246794583ce4ee1031e1da71895";
-  nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${commitRev}.tar.gz";
-    sha256 = "1zn1lsafn62sz6azx6j735fh4vwwghj8cc9x91g5sx2nrg23ap9k";
-  };
-  pkgs = import nixpkgs { config = { allowUnfree = true; }; };
-  pythonPackages = pkgs.python36Packages;
-in
+{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python312Packages }:
+
 pkgs.mkShell {
   buildInputs = [
     pythonPackages.numpy 
@@ -18,6 +8,19 @@ pkgs.mkShell {
   ];
   
   shellHook = ''
-    echo "execute any bash commands before starting jupyterlab";
+    if [ ! -f $HOME/.dockerbuildphase ]; then
+      touch $HOME/.dockerbuildphase
+      export DOCKER_BUILD_PHASE=true
+    fi
+    
+    if [ "$DOCKER_BUILD_PHASE" = true ]; then
+      echo "Do some action in build phase"
+    fi
+    
+    if [ "$DOCKER_BUILD_PHASE" = false ]; then
+      echo "Do some action in run phase"
+    fi
+    
+    echo "Do some action in both phases"
   '';
 }

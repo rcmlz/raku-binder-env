49fc3bd62d26fa62d83038cd2935d7666028cbe5
rcmlz <rcmlz@github.com>
2026-01-15 12:13:39 +0100

    Ollama

--- postBuild.d/03_ollama.sh
+++ postBuild.d/03_ollama.sh
@@ -6,4 +6,4 @@ echo "instruction taken from https://docs.ollama.com/linux"
 curl -fsSL https://ollama.com/download/ollama-linux-amd64.tar.zst | sudo tar x -C .
 
 #running as local user:    
-ollama serve
+#ollama serve
